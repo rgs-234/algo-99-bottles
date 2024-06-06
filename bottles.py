4f4272820c8504fc922239d6598b3f497d13c4ac
rgs-234 <rachelgsnyder@gmail.com>
2024-06-06 18:05:16 -0500

    complete 99 bottles assignment

--- bottles.py
+++ bottles.py
@@ -1,7 +1,17 @@
-def bottle_song():
-	# write your code here!
-	pass
+def bottle_song(num_beers):
 
-bottle_song()
+	if num_beers < 2:
+		print("That's not enough beers.")
+	elif num_beers > 2:
+		for idx in range(num_beers, 2, -1):
+			print(f"{idx} bottles of beer on the wall, {idx} bottles of beer.")
+			print(f"Take one down and pass it around, {idx - 1} bottles of beer on the wall.")
 
-####kldjsfalkfdajalskdfjdfls
+	print("""2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall.
+1 bottle of beer on the wall, 1 bottle of beer.
+Take one down and pass it around, no more bottles of beer on the wall.
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.""")
+	
+bottle_song(99)
